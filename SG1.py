756b928790b38e1ebf8ed20f99bbb7590f81b0c2
ViciousArticWolf <140591954+ViciousArticWolf@users.noreply.github.com>
2023-08-01 20:27:45 +0530

    Add files via upload
    This code sets up a basic Snake game using the turtle module. The snake moves using the arrow keys, and the game ends if the snake hits the boundary or itself. When the snake eats the food (black circle), it grows longer. The game continues until you close the window. To play the game, click on the game window first, and then use the arrow keys to control the snake.

---
+++ SG1.py
@@ -0,0 +1,130 @@
+import turtle
+import time
+import random
+
+delay = 0.1
+
+# Set up the screen
+window = turtle.Screen()
+window.title("Snake Game")
+window.bgcolor("white")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("black")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+window.listen()
+window.onkeypress(go_up, "Up")
+window.onkeypress(go_down, "Down")
+window.onkeypress(go_left, "Left")
+window.onkeypress(go_right, "Right")
+
+# Main game loop
+while True:
+    window.update()
+
+    # Check for a collision with the border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        segments.clear()
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("green")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    # Check for head collisions with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            segments.clear()
+
+    time.sleep(delay)
+
+window.mainloop()
